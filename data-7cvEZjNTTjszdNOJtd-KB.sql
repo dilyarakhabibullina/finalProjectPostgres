a12c440eaa7ca71b4200b73c1b8c96b476edeccd
dilyarakhabibullina <47914249+dilyarakhabibullina@users.noreply.github.com>
2022-10-21 00:54:24 +0300

    Delete data-7cvEZjNTTjszdNOJtd-KB.sql

--- data-7cvEZjNTTjszdNOJtd-KB.sql
+++
@@ -1,7 +0,0 @@
-SELECT players.id, first_name, second_name, results.scores, what_was_hurt FROM players
-LEFT JOIN results ON players.id = results.player_id
-LEFT JOIN injuries ON injury_id = injuries.id ORDER BY results.scores DESC;
-
-
-SELECT free_throws, faults FROM results 
-WHERE player_id = (SELECT players.id FROM players WHERE first_name ='Noah' AND second_name = 'Baldwin');
